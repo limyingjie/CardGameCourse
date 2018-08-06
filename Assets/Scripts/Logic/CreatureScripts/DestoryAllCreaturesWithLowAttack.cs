8b422dbcc040620bd7b9bbbcf8cc269c6a44e5ee
wyf <614119314@qq.com>
2018-08-06 14:49:45 +0800

    test

---
+++ Assets/Scripts/Logic/CreatureScripts/DestoryAllCreaturesWithLowAttack.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryAllCreaturesWithLowAttack : CreatureEffect {
+	public DestoryAllCreaturesWithLowAttack(Player owner, CreatureLogic creature, int specialAmount): base(owner, creature, specialAmount)
+	{}
+	public override void WhenACreatureIsPlayed(){
+		CreatureLogic[] EnemyCreatures = TurnManager.Instance.whoseTurn.otherPlayer.table.CreaturesOnTable.ToArray();
+		foreach (CreatureLogic cl in EnemyCreatures)
+		{
+			if (cl.Attack <=2) {
+				new DealDamageCommand(cl.ID, specialAmount, healthAfter: cl.Health - specialAmount).AddToQueue();
+				cl.Health -= specialAmount;
+			}
+
+		}
+
+
+	}
+}
