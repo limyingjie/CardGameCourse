0043da6c4d0007ac1a4ad0f2c9c354703512da9e
wyf <614119314@qq.com>
2018-08-06 16:46:13 +0800

    sss

---
+++ Assets/Scripts/Logic/SpellScripts/AverageFilter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AverageFilter : SpellEffect {
+
+	public override void ActivateEffect(int specialAmount = 0, ICharacter target = null){
+		CreatureLogic[] EnemyCreatures = TurnManager.Instance.whoseTurn.otherPlayer.table.CreaturesOnTable.ToArray();
+		foreach (CreatureLogic cl in EnemyCreatures)
+		{
+			if (cl.Attack <=2) {
+				new DealDamageCommand(cl.ID, specialAmount, healthAfter: cl.Health - specialAmount).AddToQueue();
+				cl.Health -= specialAmount;
+			}
+			if (cl.Attack >5) {
+				new DealDamageCommand(cl.ID, specialAmount, healthAfter: cl.Health - specialAmount).AddToQueue();
+				cl.Health -= specialAmount;
+			}
+		}
+
+
+		// Dmg allies
+		CreatureLogic[] AlliedCreatures = TurnManager.Instance.whoseTurn.table.CreaturesOnTable.ToArray();
+		foreach (CreatureLogic cl in AlliedCreatures)
+		{
+			if (cl.Attack <=2) {
+				new DealDamageCommand(cl.ID, specialAmount, healthAfter: cl.Health - specialAmount).AddToQueue();
+				cl.Health -= specialAmount;
+			}
+			if (cl.Attack >5) {
+				new DealDamageCommand(cl.ID, specialAmount, healthAfter: cl.Health - specialAmount).AddToQueue();
+				cl.Health -= specialAmount;
+			}
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/Logic/SpellScripts/LateSubmission.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateSubmission : SpellEffect {
+	public override void ActivateEffect(int specialAmount = 0, ICharacter target = null)
+	{
+		Player allyPlayer = TurnManager.Instance.whoseTurn;
+		new DealDamageCommand(allyPlayer.ID, specialAmount, healthAfter: allyPlayer.Health - specialAmount).AddToQueue();
+		allyPlayer.Health -= specialAmount;
+
+	}
+}
